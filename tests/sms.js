be8dee4053e6622bd4ec4c91353d72a1e5365af3
Carmen Jimenez Cabezas <cjc@tid.es>
2015-05-11 18:39:01 +0200

    wip

--- tests/sms.js
+++ tests/sms.js
@@ -2,47 +2,41 @@
 (function(window) {
   window.Tests = window.Tests || {};
 
-  var dependencies = ['/WebAPI_pf/polyfills/common/webapi_poly_common.js',
-                      '/WebAPI_pf/polyfills/sms/sms.js'];
+  window.Tests['sms'] = {
+    dependencies : [
+      '/WebAPI_pf/polyfills/common/webapi_poly_common.js',
+      '/WebAPI_pf/polyfills/sms/sms.js'],
 
-  window.Tests['sms'] =
-    LazyLoader.dependencyLoad(dependencies).then(() => {
-      var log = window.Tests.log.bind(undefined, 'smsTest');
-      return {
-        runTest: function() {
-          function abort(e) {
-            throw e;
-          }
+    runTest: function() {
+      var log = window.Tests.log.bind(undefined, 'sms');
+      var abort = window.Tests.abort;
 
-          var _mozSMS = window.navigator.mozMobileMessage;
+      var _mozSMS = window.navigator.mozMobileMessage;
 
-          function testSend() {
-            var recipients = '682681246';
-            var content = 'POLYFILL testing message';
-            var options = {};
+      function testSend() {
+        var recipients = '676525594';//'682681246';
+        var content = 'POLYFILL testing message';
+        var options = {};
 
-            log('window.navigator.mozMobileMessage defined!');
-            var lock = _mozSMS.send(recipients, content, options);
+        log('window.navigator.mozMobileMessage defined!');
+        var lock = _mozSMS.send(recipients, content, options);
 
-            lock.then(success => {
-              log('Successfuly sent msg' + JSON.stringify(success));
-            }).catch(error => {
-              log('Failured sending msg' + JSON.stringify(error));
-            });
-          }
+        lock.then(success => {
+          log('Successfuly sent msg' + JSON.stringify(success));
+        }).catch(error => {
+          log('Failured sending msg' + JSON.stringify(error));
+        });
+      }
 
-          try {
-            log('Starting sms polyfill tests');
-            window.navigator.mozMobileMessage ||
-              abort('window.navigator.mozMobileMessage not defined.');
+      try {
+        log('Starting sms polyfill tests');
+        window.navigator.mozMobileMessage ||
+          abort('window.navigator.mozMobileMessage not defined.');
 
-            testSend();
-          } catch (e) {
-            log("Finished early with " + e);
-          }
-
-        }
-      };
-    });
-
+        testSend();
+      } catch (e) {
+        log("Finished early with " + e);
+      }
+    }
+  };
 })(window);
